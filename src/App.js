8e928992cc52d639ea82d51124d786aacbb84b9d
Ben Hessel <hesselfitness@gmail.com>
2022-10-07 09:58:59 -0400

    flexbox article demo done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="item i-1">1</div>
+      <div className="item i-2">2</div>
+      <div className="item i-3">3</div>
+      <div className="item i-4">4</div>
+      <div className="item i-5">1</div>
+      <div className="item i-6">2</div>
+      <div className="item i-7">3</div>
+      <div className="item i-8">4</div>
     </div>
   );
 }
